fc5ca377435ad8b1812368dc2506d8fa68f542ab
Gabby Recny <gjtrecny@gmail.com>
2022-05-16 16:55:04 -0500

    Add basic layout for text area

--- src/TextArea/TextArea.js
+++ src/TextArea/TextArea.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import './TextArea.scss';
 
 const TextArea = () => {
+    const [ textAreaInput, setTextAreaInput ] = useState('')
+
+    const submitResponse = (event) => {
+        event.preventDefault()
+        console.log('Response submitted')
+    }
     return (
-        <>
-            <p>Text Area</p>
-        </>
+        <section className='form-section'>
+            <form>
+                <label htmlFor='textArea'>Enter prompt</label>
+                <section className='input-area'>
+                    <textarea 
+                        type='text'
+                        className='text-area'
+                        // rows='20'
+                        // columns='50'
+                        value={textAreaInput} 
+                        onChange={e => setTextAreaInput(e.target.value)} 
+                    />
+                </section>
+                <button
+                    className='submit-btn'
+                    type='submit'
+                    onClick={e => submitResponse(e)}
+                    >Submit</button>
+            </form>
+        </section>
     )
 }
+
 
 export default TextArea;
