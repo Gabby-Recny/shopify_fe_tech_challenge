55ab8734336c58ad9862cc3805f9cc8b4bc97b73
Gabby Recny <gjtrecny@gmail.com>
2022-05-16 16:08:50 -0500

    Create TextArea and Responses components

---
+++ src/Responses/Responses.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Responses.scss';
+
+const Responses = () => {
+    return (
+        <h2>Responses</h2>
+    )
+}
+
+export default Responses;
---
+++ src/TextArea/TextArea.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './TextArea.scss';
+
+const TextArea = () => {
+    return (
+        <>
+            <p>Text Area</p>
+        </>
+    )
+}
+
+export default TextArea;
