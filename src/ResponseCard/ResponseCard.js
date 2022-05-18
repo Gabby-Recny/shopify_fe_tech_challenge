a5df5f21ce4720ca7b639772bb86eddf784af921
Gabby Recny <gjtrecny@gmail.com>
2022-05-17 21:01:15 -0500

    Format Response Cards

---
+++ src/ResponseCard/ResponseCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ResponseCard.scss';
+
+const ResponseCard = ({id, generatedResponse, prompt}) => {
+    return (
+        <article className='response-card'>
+            <section className='response-layout'>
+                <label htmlFor='prompt'>Prompt:</label>
+                <span name='prompt' id='prompt'>{prompt}</span>
+            </section>
+            <br></br>
+            <section className='response-layout'>
+                <label htmlFor='written-response'>Response:</label>
+                <span name='written-response' id='written-response'>
+                    {generatedResponse}
+                </span>
+            </section>
+
+        </article>
+
+    )
+}
+
+export default ResponseCard;
