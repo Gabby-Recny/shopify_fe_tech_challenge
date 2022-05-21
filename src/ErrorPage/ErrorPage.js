6a54e5208aa8e1dd0bcbadd92afc5dc2a146d3f6
Gabby Recny <gjtrecny@gmail.com>
2022-05-21 12:46:15 -0500

    Add and style back home button

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -7,7 +7,8 @@ const ErrorPage = () => {
         <section className='error-page'>
             <h2>404 Error</h2>
             <img className='error-page-img' src={crystalBall} alt='Crystal ball vector for a 404 error'/>
-            <h4 className='error-page-msg'>We're sorry, there seems to be a 404 Error. <br></br>The crystyal ball says to refresh or try again later.</h4>
+            <h3 className='error-page-msg'>We're sorry, there seems to be a 404 Error. <br></br>The crystyal ball says to refresh or try again later.</h3>
+            <button className='error-page-btn'>Go Home</button>
         </section>
     )
 }
