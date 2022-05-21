f0d83d3d514953c0b159dd979b22634713b15d19
Gabby Recny <gjtrecny@gmail.com>
2022-05-21 12:22:55 -0500

    Style error page

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -4,9 +4,11 @@ import './ErrorPage.scss';
 
 const ErrorPage = () => {
     return (
-        <>
-            <img src={crystalBall} alt='Crystal ball vector for a 404 error'/>
-        </>
+        <section className='error-page'>
+            <h2>404 Error</h2>
+            <img className='error-page-img' src={crystalBall} alt='Crystal ball vector for a 404 error'/>
+            <h4 className='error-page-msg'>We're sorry, there seems to be a 404 Error. <br></br>The crystyal ball says to refresh or try again later.</h4>
+        </section>
     )
 }
 
