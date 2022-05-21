cb36bc8b1ddd8ef96b914f6c3d0b425bc99effcb
Gabby Recny <gjtrecny@gmail.com>
2022-05-21 13:22:04 -0500

    Add Link to Error page

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import crystalBall from '../assets/crystal-ball.png';
 import './ErrorPage.scss';
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <section className='error-page'>
             <h2>404 Error</h2>
             <img className='error-page-img' src={crystalBall} alt='Crystal ball vector for a 404 error'/>
-            <h3 className='error-page-msg'>We're sorry, there seems to be a 404 Error. <br></br>The crystyal ball says to refresh or try again later.</h3>
-            <button className='error-page-btn'>Go Home</button>
+            <h3 className='error-page-msg'>We're sorry, there seems to be a 404 Error. <br></br>The crystyal ball says to refresh or press the  magic button below.</h3>
+            <Link to="/">
+                <button className='error-page-btn'>Go Home</button>
+            </Link>
         </section>
     )
 }
