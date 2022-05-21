e34436b7c78578b1167946a6ed8adc313b228976
Gabby Recny <gjtrecny@gmail.com>
2022-05-21 12:07:45 -0500

    Create 404 Error Page folder

---
+++ src/ErrorPage/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import crystalBall from './crystal-ball.png';
+import './ErrorPage.scss';
+
+const ErrorPage = () => {
+    return (
+        <>
+            <img src={crystalBall} alt='Crystal ball vector for a 404 error'/>
+        </>
+    )
+}
+
+export default ErrorPage;
